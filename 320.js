d23cbbdb30e484dded16bc762601fca919a208d1
Xasmizani <96251766+Xasmizani@users.noreply.github.com>
2021-12-16 22:40:58 +0700

    Create 320.js

---
+++ 320.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'e191cd323dde19bf68cc5e41c249a3e3',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedformats.com/e191cd323dde19bf68cc5e41c249a3e3/invoke.js"></scr' + 'ipt>');
