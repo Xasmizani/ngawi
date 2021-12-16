63936911a02e60b297ca0ca79bb6300570305c22
Xasmizani <96251766+Xasmizani@users.noreply.github.com>
2021-12-16 22:40:03 +0700

    Create 300.js

---
+++ 300.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'fc499d05e1dcfa8e16a05167e9813399',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedformats.com/fc499d05e1dcfa8e16a05167e9813399/invoke.js"></scr' + 'ipt>');
